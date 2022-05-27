5b0945c0c09082f9755f0bc219cdecaa8c2cd56c
wanshengqiang <wan624773262@gmail.com>
2022-05-27 18:53:34 +0800

    fix bug

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const path = require('path');
 const del = require('del');
 const babel = require('gulp-babel')
 const ts = require('gulp-typescript');
+const through = require('through2');
 const tsconfig = require('./tsconfig.json');
 
 function clean() {
@@ -42,9 +43,28 @@ function buildUMD() {
     .pipe(gulp.dest('lib/umd/'));
 }
 
+function generatePackageJSON() {
+  return gulp
+    .src('./package.json')
+    .pipe(
+      through.obj((file, enc, cb) => {
+        const rawJSON = file.contents.toString();
+        const parsed = JSON.parse(rawJSON);
+        delete parsed.scripts;
+        delete parsed.devDependencies;
+        delete parsed.publishConfig;
+        delete parsed.files;
+        const stringified = JSON.stringify(parsed, null, 2);
+        file.contents = Buffer.from(stringified);
+        cb(null, file);
+      }),
+    )
+    .pipe(gulp.dest('./lib/'));
+}
+
 exports.default = gulp.series(
   clean,
   buildES,
-  gulp.parallel(buildCJS, buildUMD),
+  gulp.parallel(buildCJS, buildUMD, generatePackageJSON),
 );
 
