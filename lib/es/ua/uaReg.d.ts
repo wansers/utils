cec2151005652e8ad76fbf1740846f3551315b61
wanshengqiang <wan624773262@gmail.com>
2022-05-27 18:27:06 +0800

    update ignore

--- lib/es/ua/uaReg.d.ts
+++
@@ -1,9 +0,0 @@
-declare const isWindows: boolean;
-declare const isMacOs: boolean;
-declare const isMobile: boolean;
-declare const isAndroid: boolean;
-declare const isIOS: boolean;
-declare const isIPhone: boolean;
-declare const isIPad: boolean;
-declare const isIPhoneX: () => boolean;
-export { isWindows, isMacOs, isMobile, isAndroid, isIOS, isIPad, isIPhone, isIPhoneX, };
